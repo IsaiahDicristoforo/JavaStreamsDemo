1521738cf3969d89156a399b7e8f7e3507529145
Isaiah Dicristoforo <48969369+IsaiahDicristoforo@users.noreply.github.com>
2020-05-25 15:33:01 -0400

    More streams

--- src/Streams/Main.java
+++ src/Streams/Main.java
@@ -1,6 +1,7 @@
 package Streams;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -9,19 +10,50 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Prints out each produce Item. Terminal operation.
-        System.out.println("Produce Items");
-        createStreamFromArray().forEach(System.out::println);
+        Integer[] randomAges = {10,20,19,32};
 
-        List<ProduceItem> items = Arrays.asList(new ProduceItem("Orange",12,ProduceType.FRUIT),
-                new ProduceItem("Lettuce",1,ProduceType.VEGTABLE));
+        List<String> ages = Stream.of(randomAges) //We start out with a stream of ints than print switch to a stream of strings
+                .map(age -> "Age: " + age)
+                .collect(Collectors.toList());
+        ages.forEach(System.out::println);
 
-        items.stream().map(item -> {
-            item.setCost(21);
-            return item;
-        }).collect(Collectors.toList());
+        //Map accepts a function, peek accepts a consumer.
 
-        items.forEach(System.out::println);
+        Stream<Integer> infiniteStream = Stream.iterate(2, i -> i * 2);
+
+        List<Integer> collect = infiniteStream
+                .skip(3)
+                .limit(5)
+                .peek(System.out::println)
+                .collect(Collectors.toList());
+
+
+        Stream<Integer> fizzBuzzStream = Stream.iterate(1,i -> i + 1);
+
+        System.out.println("Max is: " + fizzBuzzStream
+                .limit(100)
+                .max((num1, num2) -> num1 - num2)
+                .get());
+
+        List<ProduceItem> produceItems = Arrays.asList(
+                new ProduceItem("Apple",12.99,ProduceType.FRUIT),
+                new ProduceItem("Squash",20.99,ProduceType.VEGTABLE),
+                new ProduceItem("Nuts",2,ProduceType.OTHER)
+                );
+        System.out.println("The most expensive produce item is the " + produceItems.stream().max(Comparator.comparingDouble(ProduceItem::getCost)).get().getName());
+        System.out.println("The least expensive produce item is the " + produceItems.stream().min(Comparator.comparingDouble(ProduceItem::getCost)).get().getName());
+
+        System.out.println("The average price is " + produceItems.stream().mapToDouble(ProduceItem::getCost).average().getAsDouble());
+        System.out.println("The sum of all the produce items is " + produceItems.stream().mapToDouble(ProduceItem::getCost).sum());
+
+
+
+
+
+
+
+
+
 
 
 
